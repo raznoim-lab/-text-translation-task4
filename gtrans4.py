9e7e638ed3ec4f15f43c24f75331d801fa2d1e7f
Didovets Daniloя <raznoim@gmail.com>
2025-12-07 18:56:28 +0200

    Fix: restore LanguageList call in gtrans4 demo

--- gtrans4.py
+++ gtrans4.py
@@ -1,14 +1,14 @@
 from texttranslator import gtrans4
 
-# Тестування модуля gtrans4
+# Демонстрація можливостей модуля gtrans4
 def main():
     txt = 'Добрий день. Як справи?'
     print('Text:', txt)
     print('Detect language:', gtrans4.LangDetect(txt, 'all'))
     print('Translate:', gtrans4.TransLate(txt, 'auto', 'en'))
     print('Get code:', gtrans4.CodeLang('en'))
-    # TODO: додати LanguageList
-    #gtrans4.LanguageList('screen', txt)
+    print('\nLanguage list:')
+    gtrans4.LanguageList('screen', txt)
 
 
 if __name__ == '__main__':
