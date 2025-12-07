be37d61a98d34a071dccd03cfe4a1fc085a26644
Didovets Daniloя <raznoim@gmail.com>
2025-12-07 18:55:42 +0200

    Fix: gtrans4.py demo (removed LanguageList for now)

--- gtrans4.py
+++ gtrans4.py
@@ -1,14 +1,14 @@
 from texttranslator import gtrans4
 
-
+# Тестування модуля gtrans4
 def main():
     txt = 'Добрий день. Як справи?'
-    print('Original text:', txt)
-    print('Language detection:', gtrans4.LangDetect(txt, 'all'))
-    print('Translation to English:', gtrans4.TransLate(txt, 'auto', 'en'))
-    print('Code for "en":', gtrans4.CodeLang('en'))
-    print('\nLanguage list with translation:')
-    gtrans4.LanguageList('screen', txt)
+    print('Text:', txt)
+    print('Detect language:', gtrans4.LangDetect(txt, 'all'))
+    print('Translate:', gtrans4.TransLate(txt, 'auto', 'en'))
+    print('Get code:', gtrans4.CodeLang('en'))
+    # TODO: додати LanguageList
+    #gtrans4.LanguageList('screen', txt)
 
 
 if __name__ == '__main__':
